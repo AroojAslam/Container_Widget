39e935743fa2cc78349a585f3790c35406fe8bb8
AroojAslam <101873027+AroojAslam@users.noreply.github.com>
2023-07-13 20:16:11 +0500

    create priject

---
+++ container_widget/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ContainerWidget());
+}
+
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
